28ab0c8ef90a19a5593d57051cd269433b98b2dd
rafaamary <rafaela.amary@outlook.com>
2023-02-23 13:51:49 -0300

    New Components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral> </BarraLateral>
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioTarefa @aoSalvartarefa="salvarTarefa"> </FormularioTarefa>
+      <!-- Lista de Tarefas -->
+      <div class="listaTarefas">
+        <TarefaLista v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"></TarefaLista>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import FormularioTarefa from "./components/FormularioTarefa.vue";
+import TarefaLista from "./components/TarefaLista.vue";
+import ITarefa from "./interfaces/ITarefa";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: {
+      BarraLateral,
+      FormularioTarefa,
+      TarefaLista
+    },
+    data () {
+      return {
+        tarefas: [] as ITarefa[]
+      }
+    },
+    methods: {
+      salvarTarefa (tarefa: ITarefa) {
+        this.tarefas.push(tarefa)
+      },
+    }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.listaTarefas {
+  padding: 1.25rem;
 }
 </style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.jpg" alt="">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background: #FF74B1;
+    width: 100%;
+    height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
